c4454d26ac494856ee44402553b039dbb189502b
ANVANE Marcel <53498389+MarcelWeb007@users.noreply.github.com>
2022-05-15 22:19:09 +0200

    test design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
